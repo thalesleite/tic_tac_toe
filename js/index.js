bb186e001ca7a9b5d222e5e7fdac222e5df1d971
thalesleite <thalesaleite@gmail.com>
2024-05-02 16:51:59 +0100

    finish game logic only for console mode, needs to create the UI for the game

--- js/index.js
+++ js/index.js
@@ -14,7 +14,10 @@ function Gameboard() {
   const getBoard = () => board
 
   const markBoard = (row, collumn, player) => {
-    if (board[row][collumn].getValue() !== 0) return
+    if (board[row][collumn].getValue() !== 0) {
+      console.log("This square is already occupied. Please choose another one.")
+      return
+    }
 
     board[row][collumn].addToken(player)
     printBoard()
@@ -49,7 +52,7 @@ function Square() {
   }
 }
 
-function GameController(playerOne = "Player Oner", playerTwo = "Player Two") {
+function GameController(playerOne = "Player One", playerTwo = "Player Two") {
   const board = Gameboard()
 
   const players = [
@@ -83,14 +86,18 @@ function GameController(playerOne = "Player Oner", playerTwo = "Player Two") {
     )
     board.markBoard(row, collumn, getActivePlayer().token)
 
-    if (gameFinished()) {
+    const gameAnalises = checkGameFinished()
+
+    if (gameAnalises.isFinished) {
       console.log("Game finished.")
-      // const winner = getWinner()
-      // if (winner) {
-      //   console.log(`${winner.name} wins!`)
-      // } else {
-      //   console.log("It's a draw!")
-      // }
+      const winner = getWinner(gameAnalises.winner)
+
+      if (winner.token !== "DRAW") {
+        console.log(`The winner is: ${winner.name}`)
+      } else {
+        console.log(`It is a DRAW!`)
+      }
+
       return
     }
 
@@ -98,7 +105,7 @@ function GameController(playerOne = "Player Oner", playerTwo = "Player Two") {
     newRound()
   }
 
-  const gameFinished = () => {
+  const checkGameFinished = () => {
     const currentBoard = board.getBoard()
 
     for (let index = 0; index < currentBoard.length; index++) {
@@ -109,7 +116,10 @@ function GameController(playerOne = "Player Oner", playerTwo = "Player Two") {
         currentBoard[index][0].getValue() === currentBoard[index][2].getValue()
       ) {
         // Horizontal win
-        return true
+        return {
+          isFinished: true,
+          winner: currentBoard[index][0].getValue(),
+        }
       }
 
       if (
@@ -119,8 +129,60 @@ function GameController(playerOne = "Player Oner", playerTwo = "Player Two") {
         currentBoard[0][index].getValue() === currentBoard[2][index].getValue()
       ) {
         // Verical win
-        return true
+        return {
+          isFinished: true,
+          winner: currentBoard[0][index].getValue(),
+        }
       }
+    }
+
+    if (
+      currentBoard[0][0].getValue() !== 0 &&
+      currentBoard[0][0].getValue() === currentBoard[1][1].getValue() &&
+      currentBoard[0][0].getValue() === currentBoard[2][2].getValue()
+    ) {
+      // Diagonal win (top-left to bottom-right)
+      return {
+        isFinished: true,
+        winner: currentBoard[0][0].getValue(),
+      }
+    }
+    if (
+      currentBoard[0][2].getValue() !== 0 &&
+      currentBoard[0][2].getValue() === currentBoard[1][1].getValue() &&
+      currentBoard[0][2].getValue() === currentBoard[2][0].getValue()
+    ) {
+      // Diagonal win (top-right to bottom-left)
+      return {
+        isFinished: true,
+        winner: currentBoard[0][2].getValue(),
+      }
+    }
+
+    // check for draw
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (currentBoard[i][j].getValue() === 0) {
+          return {
+            isFinished: false,
+            winner: 0,
+          }
+        }
+      }
+    }
+    // Draw
+    return {
+      isFinished: true,
+      winner: "DRAW",
+    }
+  }
+
+  const getWinner = (token) => {
+    if (token === "X") return players[0]
+    if (token === "O") return players[1]
+    return {
+      name: "None",
+      token: "DRAW",
     }
   }
 
@@ -132,8 +194,29 @@ function GameController(playerOne = "Player Oner", playerTwo = "Player Two") {
 }
 
 const game = GameController()
+
+// Simulate Player's One winner
+// game.playRound(2, 0)
+// game.playRound(1, 1)
+// game.playRound(2, 1)
+// game.playRound(0, 2)
+// game.playRound(2, 2)
+
+// Simulate Player's Two winner
+// game.playRound(2, 0)
+// game.playRound(1, 1)
+// game.playRound(0, 0)
+// game.playRound(1, 0)
+// game.playRound(0, 2)
+// game.playRound(1, 2)
+
+// Simulate Draw
 game.playRound(2, 0)
 game.playRound(1, 1)
-game.playRound(2, 1)
+game.playRound(0, 0)
+game.playRound(1, 0)
 game.playRound(0, 2)
+game.playRound(0, 1)
+game.playRound(1, 2)
 game.playRound(2, 2)
+game.playRound(2, 1)
